c0c9fc4ca1cc2af455f5b999f51e54cff84d88e4
Adereti <kunlesxo>
2025-01-22 06:21:34 -0800

    Webstore/models.py, Webstore/admin.py

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
+import random
+import string
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
@@ -55,7 +57,7 @@ class CartItem(models.Model):
     
 class Cart(models.model):
     id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(user, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     product_item = models.ForeignKey(CartItem, on_delete=models.CASCADE)
     
     @property
@@ -63,3 +65,38 @@ class Cart(models.model):
         return self.product_item.cost
         
 
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    code = models.CharField(max_length=10)
+
+
+    def Save(self, *args, **kwargs):
+        if not self.code:
+            self.code = self.generate_order_code()
+        super().save(*args, **kwargs)
+
+
+
+    def generate_order_code(self):
+        """Generate a unique 10-charater alphanumeric code."""  
+        while True:
+            code = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
+            if not Order.objects.filter(code=code).exists():
+                return code
+
+class Payment(models.Model):
+    id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reference_id = models.CharField(max_length=12 ,unique=True)
+    status = models.CharField(max_length=20 , default="processings")
+    time = models.DateTimeField(auto_now=True)
+
+
+    def Save(self, *args, **kwargs):
+        if self.order:
+            self.amount = self.order.cart.total_cost
+            self.user = self.order.cart.user
+        super().save(*args, **kwargs)    
